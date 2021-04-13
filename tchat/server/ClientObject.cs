b61e068087f5e9f74e0d1e197b03d6e9dce59473
wenny380 <52930464+wenny380@users.noreply.github.com>
2021-04-14 01:13:51 +0300

    Add files via upload

---
+++ tchat/server/ClientObject.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+namespace server
+{
+    public class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerObject server; // объект сервера
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                // получаем имя пользователя
+                string message = GetMessage();
+                userName = message;
+
+                message = userName + " вошел в чат";
+                // посылаем сообщение о входе в чат всем подключенным пользователям
+                server.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+                // в бесконечном цикле получаем сообщения от клиента
+              
+                while (true)
+              {
+                    try
+                    {
+                        message = GetMessage();
+                        if (message == "") continue;
+                        message = String.Format("{0}: {1}", userName, message);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                    }
+                    catch
+                    {
+                        message = String.Format("{0}: покинул чат", userName);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+               }
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                server.RemoveConnection(this.Id);
+                Close();
+            }
+        }
+
+        // чтение входящего сообщения и преобразование в строку
+        private string GetMessage()
+        {
+            byte[] data = new byte[64]; // буфер для получаемых данных
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            while (Stream.DataAvailable) 
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+           }
+          
+
+            return builder.ToString();
+        }
+
+        // закрытие подключения
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+}
